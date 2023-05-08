a33577dc0a16239ba515dd34d360ac6410f4f18a
macbook <jnrfotio@gmail.com>
2023-05-08 18:49:13 +0200

    create Client Restcontroller

--- src/main/java/com/adias/demo/controller/implementations/ClientController.java
+++ src/main/java/com/adias/demo/controller/implementations/ClientController.java
@@ -1,28 +1,30 @@
 package com.adias.demo.controller.implementations;
-
 import com.adias.demo.controller.abstractions.ClientApi;
 import com.adias.demo.dto.ClientDto;
+import com.adias.demo.services.abstractions.ClientService;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
 public class ClientController implements ClientApi {
+    private ClientService clientService;
+    public ClientController(ClientService clientService){
+        this.clientService = clientService;
+    }
     @Override
     public ClientDto save(ClientDto dto) {
-        return null;
+        return clientService.save(dto);
     }
-
     @Override
     public ClientDto findById(Integer id) {
-        return null;
+        return clientService.findById(id);
     }
-
     @Override
     public List<ClientDto> findAll() {
-        return null;
+        return clientService.findAll();
     }
-
     @Override
     public void delete(Integer id) {
-
+        clientService.delete(id);
     }
 }
