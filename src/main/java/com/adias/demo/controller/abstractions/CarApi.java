419f6e1cb1e68911b5c34938589ca17d8e23e1c5
macbook <jnrfotio@gmail.com>
2023-05-10 21:28:20 +0200

    created the class config with flickr Api and the photo service with flickr.

--- src/main/java/com/adias/demo/controller/abstractions/CarApi.java
+++ src/main/java/com/adias/demo/controller/abstractions/CarApi.java
@@ -1,24 +1,47 @@
 package com.adias.demo.controller.abstractions;
 import com.adias.demo.dto.CarDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import static com.adias.demo.utils.Constants.URL_GENERAL;
+@Api(URL_GENERAL + "/cars")
 public interface CarApi {
-    @PostMapping(value = URL_GENERAL+"/cars/create",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = URL_GENERAL+"/cars/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     //requestboby fa capire a spring dal json -> oggetto dto
+    @ApiOperation(value = "save a car", notes = "the method allows o modify a car",response = CarDto.class)
+    @ApiResponses(value ={
+            @ApiResponse(code = 200, message = "object car created/modified"),
+            @ApiResponse(code = 400, message = "object car is not valid")
+    } )
     CarDto save(@RequestBody CarDto dto);
-    @GetMapping(value = URL_GENERAL+"/cars/{id_car}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = URL_GENERAL+"/cars/{id_car}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "find a car with ID", notes = "the method allows to find a car with its ID ",response = CarDto.class)
+    @ApiResponses(value ={
+            @ApiResponse(code = 200, message = "the object was found in the database"),
+            @ApiResponse(code = 404, message = "no car exists in the database with this ID")
+    } )
     CarDto findById(@PathVariable("id_car") Integer id);
-    @GetMapping(value = URL_GENERAL+"/cars/{car_register_number}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = URL_GENERAL+"/cars/{car_register_number}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "find a car with register number", notes = "the method allows to find a car with its register number ",response = CarDto.class)
+    @ApiResponses(value ={
+            @ApiResponse(code = 200, message = "the object has been found in the database"),
+            @ApiResponse(code = 404, message = "no car exists in the database with this rgister number")
+    } )
     CarDto findByRegisterNumber(@PathVariable("car_register_number") String registerNumber);
-    @GetMapping(value = URL_GENERAL+"/cars/all",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = URL_GENERAL+"/cars/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "list of cars", notes = "the method allows to find the list of cars in the database ",responseContainer = "List<CarDto>")
+    @ApiResponses(value ={
+            @ApiResponse(code = 200, message = "the list of cars o a void list ")
+    } )
     List<CarDto> findAll();
     @DeleteMapping(value = URL_GENERAL+"/car/delete/{id_car}")
+    @ApiOperation(value = "delete a car", notes = "the method allows to delete a car with its ID ",response = CarDto.class)
+    @ApiResponses(value ={
+            @ApiResponse(code = 200, message = "the car has been deleted"),
+    } )
     void delete(@PathVariable("id_car") Integer id);
 }
