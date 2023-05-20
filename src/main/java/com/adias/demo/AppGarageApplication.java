c5154b1ad73f0f5c5a3063d4980341aaf3c7a07c
macbook <jnrfotio@gmail.com>
2023-05-20 20:21:04 +0200

    added the hibernate validator dependency

--- src/main/java/com/adias/demo/AppGarageApplication.java
+++ src/main/java/com/adias/demo/AppGarageApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppGarageApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AppGarageApplication.class, args);
 	}
 
